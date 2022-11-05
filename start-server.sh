5ef6c9b3e3abf2fe60e7b6831918a441ff0a9d60
Bohdan Shaparenko <shaparenko.bogdan@gmail.com>
2022-11-05 18:35:09 +0200

    fix client mods

--- start-server.sh
+++ start-server.sh
@@ -84,7 +84,7 @@ remove_client_mods() {
 	echo ""
 	echo ""
 	echo "Remove clients mods"
-	allClientMods=("Ding" "ReAuth" "moreoverlays" "Neat" "ToastControl" "PackMenu" "CustomWindowTitle" "BetterF3" "MouseTweaks")
+	allClientMods=("Ding" "ReAuth" "moreoverlays" "Neat" "ToastControl" "PackMenu" "CustomWindowTitle" "BetterF3" "MouseTweaks" "LegendaryTooltips" "oculus" "rubidium")
 	for f in ${allClientMods[@]}; do
         rm /minecraft-server/mods/${f}*.jar
 		rm ./mods/${f}*.jar
