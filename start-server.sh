1eeb02aadb4813bf0ad3deb880f80333b106d3d2
Bohdan <shaparenko.bogdan@gmail.com>
2022-12-01 22:49:41 +0200

    update configs

--- start-server.sh
+++ start-server.sh
@@ -12,7 +12,7 @@ esac
 export JAVA_ARGS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -Dfml.readTimeout=180 -Dfml.queryResult=confirm"
 
 export MCVER="1.19.2"
-export FORGEVER="43.1.55"
+export FORGEVER="43.1.57"
 export MAX_RAM="${RAM:-5G}"
 
 sync_jars() {
